72e11170fb5dbb6e6bca4ff1249202c61a113c8d
lbird1337 <lbird56@zohomail.com>
2025-07-02 17:34:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Убедитесь, что здесь ТОЧНОЕ название вашего репозитория
-  base: '/makeup-course-frontend/', 
+  base: '/makeup-course-frontend1/', 
   
   plugins: [react()],
 })
